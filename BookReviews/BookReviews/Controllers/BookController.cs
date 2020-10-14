3fb64ff86be9b504b575ce3ad6bb011ad434b7dd
Brian Bird <ProfBird@users.noreply.github.com>
2020-10-14 11:13:57 -0700

    Added more to the Review form and controller method

--- BookReviews/BookReviews/Controllers/BookController.cs
+++ BookReviews/BookReviews/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookReviews.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookReviews.Controllers
@@ -10,13 +11,23 @@ namespace BookReviews.Controllers
     {
         public IActionResult Index()
         {
+            
             return View();
         }
 
         // Invoke the view with form for entering a review
         public IActionResult Review()
         {
-            return View();
+            Review model = new Review();
+            User reviewer = new User();
+            model.Reviewer = reviewer;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Review(Review model)
+        {
+            return View(model);
         }
     }
 }
