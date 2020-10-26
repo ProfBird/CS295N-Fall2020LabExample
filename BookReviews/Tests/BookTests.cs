89d0e1fec024f6a160cbcb9a55ef799375ee6b74
Brian Bird <birdb@lanecc.edu>
2020-10-26 11:34:55 -0700

    New test project

---
+++ BookReviews/Tests/BookTests.cs
@@ -0,0 +1,28 @@
+using System;
+using BookReviews.Controllers;
+using BookReviews.Models;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void ReviewHttpPostTest()
+        {
+
+            // Arrange
+            var bookController = new BookController();
+            var review = new Review();
+
+            // Act - pass a model to the controller method
+            var result = (ViewResult)bookController.Review(review);
+            var modelFromResult = (Review)result.Model;
+
+            // Assert
+            // Check that the current date got added to the model
+            Assert.Equal(modelFromResult.ReviewDate.Date, DateTime.Now.Date);
+        }
+    }
+}
