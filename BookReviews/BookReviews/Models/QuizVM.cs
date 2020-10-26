3d0733636976a5272de4a2d970544fab29008f86
Brian Bird <birdb@lanecc.edu>
2020-10-26 16:58:27 -0700

    Added a quiz and quiz unit tests

---
+++ BookReviews/BookReviews/Models/QuizVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BookReviews.Models
+{
+    // View-Model for the Quiz page
+    public class QuizVM
+    {
+        // User's answer to the first question
+        public string answer1 { get; set; }
+        // Result of checking the answer
+        public string check1 { get; set; }
+
+        // Second question results
+        public string answer2 { get; set; }
+        public string check2 { get; set; }
+
+        // Third question results
+        public string answer3 { get; set; }
+        public string check3 { get; set; }
+
+        // Check the user's answers
+        public void CheckAnswers()
+        {
+            check1 = answer1 == "Victor Hugo" ? "Right" : "Wrong";
+            check2 = answer2 == "1812" ? "Right" : "Wrong";
+            check3 = answer3 == "false" ? "Right" : "Wrong";
+        }
+    }
+}
---
+++ BookReviews/Tests/AuthorTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BookReviews.Models;
+using Xunit;
+
+namespace Tests
+{
+    public class AuthorTests
+    {
+        [Fact]
+        public void QuizVMCheckRightTest()
+        {
+
+            // Arrange
+            var quiz = new QuizVM() {
+                answer1 = "Victor Hugo",
+                answer2 = "1812",
+                answer3 = "false" };
+
+            // Act - pass a model to the controller method
+            quiz.CheckAnswers();
+
+            // Assert
+            // Check that the current date got added to the model
+            Assert.True(quiz.check1 == "Right" &&
+                quiz.check2 == "Right" &&
+                quiz.check3 == "Right");
+        }
+
+        [Fact]
+        public void QuizVMCheckWrongTest()
+        {
+
+            // Arrange
+            var quiz = new QuizVM()
+            {
+                answer1 = "Charles Dickens",
+                answer2 = "1984",
+                answer3 = "true"
+            };
+
+            // Act - pass a model to the controller method
+            quiz.CheckAnswers();
+
+            // Assert
+            // Check that the current date got added to the model
+            Assert.True(quiz.check1 == "Wrong" &&
+                quiz.check2 == "Wrong" &&
+                quiz.check3 == "Wrong");
+        }
+    }
+}
