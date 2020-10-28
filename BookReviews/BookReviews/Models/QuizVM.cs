1e6f1fde5a7907ce0bf633882054d83a56e1a9e5
Brian Bird <birdb@lanecc.edu>
2020-10-28 13:18:53 -0700

    Refactored after the merge

--- BookReviews/BookReviews/Models/QuizVM.cs
+++ BookReviews/BookReviews/Models/QuizVM.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookReviews.Models
 {
     public class QuizVM
     {
-        // User's answers and whether right or wrong for three questions
+        // User's answer to the first question
         public String UserAnswer1 {get; set;}
+        // Result of checking the answer
         public String RightOrWrong1 { get; set; }
+
+        // Second question results
         public String UserAnswer2 { get; set; }
         public String RightOrWrong2 { get; set; }
+
+        // Thirds question results
         public String UserAnswer3 { get; set; }
         public String RightOrWrong3 { get; set; }
 
